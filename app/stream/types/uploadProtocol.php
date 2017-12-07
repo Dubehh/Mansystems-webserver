d1907c13a1670726055ff8e1763bff7581ff6990
dubehh <eelcoeikelboom@hotmail.com>
2017-12-07 16:05:47 +0100

    protocol changes

--- app/stream/types/uploadProtocol.php
+++ app/stream/types/uploadProtocol.php
@@ -8,6 +8,8 @@
  */
 class UploadProtocol extends Stream{
 
+    const UPLOAD_FOLDER = "uploads";
+
     private $handler;
     function __construct($resolver, $data) {
         parent::__construct($resolver, $data);
@@ -15,12 +17,25 @@ class UploadProtocol extends Stream{
     }
 
     public function onStreamRequestReceive() {
-        $method = new Method($_FILES);
-        if(!$method->isEmpty()){
-            print_r($method->getArray());
+        $fileData = new Method($_FILES);
+        $data = new Method($_POST);
+        if(!$fileData->isEmpty() && !$data->isEmpty() && ($folder = $data->fetch('targetFolder', true))!=null){
+            $file = $fileData->fetch('file');
+            $folder = dirname($_SERVER['DOCUMENT_ROOT']).'/'.self::UPLOAD_FOLDER.'/';
+            $folder = $this->forceCreate($folder);
+            $folder = $this->forceCreate($folder.$data->fetch('UUID').'/');
+            $name = substr(Security::getToken(), 0, 15).'.'.explode('.', $file['name'])[1];
+            move_uploaded_file($file['tmp_name'], $folder.$name);
+            echo $name;
         }else{
             echo 'empty array';
         }
+    }
+
+    private function forceCreate($folder){
+        if(!file_exists($folder))
+            mkdir($folder, 0777, true);
+        return $folder;
     }
 
 
