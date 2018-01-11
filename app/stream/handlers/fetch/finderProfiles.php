a100b86d95b12bcb281f25fbfc5d3877b3e48413
Dubehh <eelcoeikelboom@hotmail.com>
2018-01-11 11:57:22 +0100

    bugfix

--- app/stream/handlers/fetch/finderProfiles.php
+++ app/stream/handlers/fetch/finderProfiles.php
@@ -21,7 +21,7 @@ class FinderProfiles extends DataHandler implements IStreamResponse{
             ->innerJoin("$playerTable ON " . FinderProfiles::TABLE_NAME . ".playerid = $playerTable.id")
             ->select("$playerTable.uuid");
 
-        $folder = dirname($_SERVER['DOCUMENT_ROOT']).'/uploads/finder/';
+        $folder = $_SERVER['DOCUMENT_ROOT'].'/upload/finder/';
         $index = 0;
         foreach ($data->getIterator() as $row) {
             $response[$index] = $row;
